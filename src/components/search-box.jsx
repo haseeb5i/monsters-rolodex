c98a43d4b95afd52926d260a3bfba2b60662d7f0
Raven Redstain <ravenredstain@outlook.com>
2021-08-04 20:07:15 +0500

    whatever

--- src/components/search-box.jsx
+++
@@ -1,9 +0,0 @@
-import "./search-box.css";
-
-const SearchBox = ({ placeholder, onChangeHandler }) => {
-  return (
-    <input className='search' type="search" placeholder={placeholder} onChange={onChangeHandler} />
-  );
-};
-
-export default SearchBox;
